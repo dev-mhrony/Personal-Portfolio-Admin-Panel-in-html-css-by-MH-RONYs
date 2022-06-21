74fcbf99c51bbb33b6c25ab133c1fb0f500c623c
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-22 02:26:35 +0600

    Last update : 22/06/2022

--- contact.php
+++ contact.php
@@ -54,10 +54,58 @@
 
                     <!-- Dashboard content -->
                     <div class="row">
-                        <div class="col-lg-8">
+                        <div class="col-lg-12">
                             <!-- Quick stats boxes -->
                             <div class="row">
 
+
+                                <div class="panel panel-flat">
+                                    <div class="panel-heading">
+                                        <h5 class="panel-title">Welcome to Contact Section</h5>
+                                        <div class="heading-elements">
+                                            <ul class="icons-list">
+                                                <li><a data-action="collapse"></a></li>
+                                                <li><a data-action="reload"></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="panel-body">
+                                        Here you can change the <code>Title</code>,
+                                        <code>Short Bio</code>, <code>E-mail</code> and
+                                        <code>Phone Number</code> of your webpage.
+                                    </div>
+
+                                    <table class="table datatable-basic table-bordered table-striped table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>SN</th>
+                                                <th>Title</th>
+                                                <th>Short Bio</th>
+                                                <th>E-mail</th>
+                                                <th>Phone Number</th>
+                                                <th>Status</th>
+                                                <th class="text-center">Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>01</td>
+                                                <td>Contact Me</td>
+                                                <td>Hello This is a test Message</td>
+                                                <td>mh.rony.dhanvi@gmail.com</td>
+                                                <td>01608445456</td>
+                                                <td><span class="label label-success">Active</span></td>
+                                                <td class="text-center">
+                                                    <ul class="icons-list">
+                                                        <li><a href="#"><i class=" icon-pencil7"></i></a>
+                                                        </li>
+                                                    </ul>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
 
                             </div>
 
@@ -66,6 +114,7 @@
 
                     </div>
                     <!-- /dashboard content -->
+
 
 
                     <!-- Footer -->
